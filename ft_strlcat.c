bbad2cd074e63d2ffd9e445fa7697dd511a79e87
fgaujard <flavien.gaujard@gmail.com>
2018-11-29 12:26:10 +0100

    add2

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,19 +3,19 @@
 size_t		ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t		i;
-	size_t		t1;
-	size_t		t2;
+	size_t		dest;
+	size_t		sr;
 
 	i = 0;
-	t1 = ft_strlen(dst);
-	t2 = ft_strlen(src);
-	if (size - 1 <= t1)
-		return (t2 + size);
-	while (t1 + i < size - 1)
+	dest = ft_strlen(dst);
+	sr = ft_strlen(src);
+	if (size - 1 <= dest)
+		return (sr + size);
+	while (dest + i < size - 1)
 	{
-		dst[t1 + i] = src[i];
+		dst[dest + i] = src[i];
 		i++;
 	}
-	dst[t1 + i] = '\0';
-	return (t1 + t2);
+	dst[dest + i] = '\0';
+	return (dest + sr);
 }
