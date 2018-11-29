e2d81f27770db8a73f215e5a8d0df7841f292e63
fgaujard <flavien.gaujard@gmail.com>
2018-11-29 12:01:18 +0100

    add

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,33 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fgaujard <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/13 12:58:20 by fgaujard          #+#    #+#             */
-/*   Updated: 2018/11/28 16:45:44 by fgaujard         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t l)
+size_t		ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t size_dest;
-	size_t size_src;
-	size_t i;
+	size_t		i;
+	size_t		t1;
+	size_t		t2;
 
 	i = 0;
-	size_dest = ft_strlen(dest);
-	size_src = ft_strlen(src);
-	if (size_dest >= (l - 1))
-		return (size_src + size_dest);
-	while ((size_dest + i) < (l - 1))
+	t1 = ft_strlen(dst);
+	t2 = ft_strlen(src);
+	if (size - 1 <= t1)
+		return (t2 + size);
+	while (t1 + i < size - 1)
 	{
-		dest[size_dest + i] = src[i];
+		dst[t1 + i] = src[i];
 		i++;
 	}
-	dest[size_dest + i] = '\0';
-	return (size_dest + size_src);
+	dst[t1 + i] = '\0';
+	return (t1 + t2);
 }
--- ft_strstr.c
+++ ft_strstr.c
@@ -1,38 +1,22 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fgaujard <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/12 18:02:24 by fgaujard          #+#    #+#             */
-/*   Updated: 2018/11/28 17:23:29 by fgaujard         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-char	*ft_strstr(const char *str, const char *to_find)
+char	*ft_strstr(char *str, char *to_find)
 {
-	int len_str;
-	int len_to_find;
 	int i;
 	int j;
 
 	i = 0;
-	len_str = ft_strlen(str);
-	len_to_find = ft_strlen(to_find);
-	while (i < (len_str - len_to_find + 1))
+	while (str[i] != '\0')
 	{
 		j = 0;
-		while (j < len_to_find)
+		while (to_find[j] == str[i + j])
 		{
-			if (str[i + j] != to_find[j])
-				break ;
+			if (to_find[j + 1] == '\0')
+			{
+				return (str + i);
+			}
 			j++;
 		}
-		if (j == len_to_find)
-			return ((char *)(str + i));
 		i++;
 	}
 	return (0);
